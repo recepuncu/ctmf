79d6c9c896a387d46f121c28dff605ba99328dfb
RECEP UNCU <recepuncu@gmail.com>
2016-08-09 10:27:44 +0300

    a

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $dXVpZA = md5(uniqid(mt_rand(), true));
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" ng-app="Ym9yc2E">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="tr" ng-app="Ym9yc2E">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Cep Telefonu - Modelleri & Fiyatları</title>
