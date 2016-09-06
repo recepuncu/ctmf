fc254b68d8614f99ad8e0ac2c1358d61e1ec8533
RECEP UNCU <recepuncu@gmail.com>
2016-09-06 11:25:30 +0300

    a

--- index.php
+++ index.php
@@ -25,7 +25,8 @@ $dXVpZA = md5(uniqid(mt_rand(), true));
 <title>Cep Telefonu ve Tablet Fiyatları</title>
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link href="bootstrap.custom.min.css" rel="stylesheet" />
-<meta name="theme-color" content="#d9831f">
+<link rel="icon" type="image/x-icon" href="favicon.ico" />
+<meta name="theme-color" content="#d9831f" />
 </head>
 
 <body ng-controller="Ym9yc2FjdHJs" ng-cloak>
