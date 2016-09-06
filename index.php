2b96b2d06ce5d87a6e3672eeb13093c7f32b54af
RECEP UNCU <recepuncu@gmail.com>
2016-09-06 13:55:16 +0300

    a

--- index.php
+++ index.php
@@ -135,5 +135,14 @@ $dXVpZA = md5(uniqid(mt_rand(), true));
   ga('send', 'pageview');
 
 </script>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KT3QF9"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KT3QF9');</script>
+<!-- End Google Tag Manager -->
 </body>
 </html>
