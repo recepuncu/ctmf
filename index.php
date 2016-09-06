fec4ce227dafe12cbfdec967527db383ee0677b3
RECEP UNCU <recepuncu@gmail.com>
2016-09-06 15:11:42 +0300

    a

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $dXVpZA = md5(uniqid(mt_rand(), true));
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link href="bootstrap.custom.min.css" rel="stylesheet" />
 <link rel="icon" type="image/x-icon" href="favicon.ico" />
-<meta name="theme-color" content="#d9831f" />
+<meta name="theme-color" content="#FFC107" />
 </head>
 
 <body ng-controller="Ym9yc2FjdHJs" ng-cloak>
